4497add3379c7db2873a06cf1de610c8dae37e58
John Lu <johnlu2006@gmail.com>
2023-07-24 14:24:51 -0500

    client message list css

--- src/components/ClientMessage.js
+++ src/components/ClientMessage.js
@@ -1,47 +1,65 @@
-import { useState,useEffect } from "react"
+import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
+import "./MessageList.css"
 
 export const ClientMessageList = () => {
-   
 
-        const [messages, setMessages] = useState([])
-    
-        const localLoggedinUser = localStorage.getItem("loggedin_user")
-        const loggedinUserObject = JSON.parse(localLoggedinUser)
-    
-        useEffect(() => {
-            fetch("http://localhost:8088/messages")
-                .then(response => response.json())
-                .then((messageArray) => {
-                    const myMessages = messageArray.filter(obj=>obj.senderId===loggedinUserObject.id||obj.recipientId===loggedinUserObject.id)
-                    setMessages(myMessages)
-                })
-        }, [])
-    
-        return (
-            <article className="messages">
-                {
-                    messages.map(message => {
+
+    const [messages, setMessages] = useState([])
+
+    const localLoggedinUser = localStorage.getItem("loggedin_user")
+    const loggedinUserObject = JSON.parse(localLoggedinUser)
+
+    useEffect(() => {
+        fetch("http://localhost:8088/messages")
+            .then(response => response.json())
+            .then((messageArray) => {
+                const myMessages = messageArray.filter(obj => obj.senderId === loggedinUserObject.id || obj.recipientId === loggedinUserObject.id)
+                setMessages(myMessages)
+            })
+    }, [])
+
+    return (
+        <article className="messages">
+            {
+                messages.map(message => {
+                    if (message.senderId === loggedinUserObject.id) {
                         return (
-                            <section className="message" key={`message--${message.id}`}>
-                                
-                                   {message.senderId===loggedinUserObject.id?
-                                   <div className="message__header message__sent">🧑</div> :
-                                   <div className="message__header message__received">👩‍⚕️</div>} 
-                               
+                            <section className="message message__sent" key={`message--${message.id}`}>
+
+
+                                <div className="message__header ">🧑</div>
+
+
                                 <div className="message__content">
                                     {message.content}
                                 </div>
-                                
+
                             </section>
                         )
-                    })
-                }
-            </article>
-        )
-    
+
+                    } else {
+
+                        return (
+                            <section className="message message__received" key={`message--${message.id}`}>
+
+
+                                <div className="message__header ">👩‍⚕️</div>
+
+                                <div className="message__content">
+                                    {message.content}
+                                </div>
+
+                            </section>
+                        )
+                    }
+                })
+            }
+        </article>
+    )
+
 }
 
 export const ClientCreateMessage = () => {
-    
+
 }
