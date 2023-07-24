b912ec8080cac8f78ae0ea9521cd2b7cc01022bd
John Lu <johnlu2006@gmail.com>
2023-07-24 13:54:40 -0500

    client message list

--- src/components/ClientMessage.js
+++ src/components/ClientMessage.js
@@ -1,4 +1,44 @@
+import { useState,useEffect } from "react"
+import { Link } from "react-router-dom"
+
 export const ClientMessageList = () => {
+   
+
+        const [messages, setMessages] = useState([])
+    
+        const localLoggedinUser = localStorage.getItem("loggedin_user")
+        const loggedinUserObject = JSON.parse(localLoggedinUser)
+    
+        useEffect(() => {
+            fetch("http://localhost:8088/messages")
+                .then(response => response.json())
+                .then((messageArray) => {
+                    const myMessages = messageArray.filter(obj=>obj.senderId===loggedinUserObject.id||obj.recipientId===loggedinUserObject.id)
+                    setMessages(myMessages)
+                })
+        }, [])
+    
+        return (
+            <article className="messages">
+                {
+                    messages.map(message => {
+                        return (
+                            <section className="message" key={`message--${message.id}`}>
+                                
+                                   {message.senderId===loggedinUserObject.id?
+                                   <div className="message__header message__sent">🧑</div> :
+                                   <div className="message__header message__received">👩‍⚕️</div>} 
+                               
+                                <div className="message__content">
+                                    {message.content}
+                                </div>
+                                
+                            </section>
+                        )
+                    })
+                }
+            </article>
+        )
     
 }
 
