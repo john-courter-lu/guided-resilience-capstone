973aa2ede76c04d06bbbdba13593600e16eb40bf
John Lu <johnlu2006@gmail.com>
2023-07-26 11:41:17 -0500

    client list shown properly

--- src/components/ClientList.js
+++ src/components/ClientList.js
@@ -1,1 +1,93 @@
-export const ClientList = () => {}
+import { useState, useEffect } from "react"
+import "./ClientList.css"
+import { useNavigate } from "react-router-dom"
+
+export const ClientList = () => {
+
+    const navigate = useNavigate()
+
+    const [clients, setClients] = useState([])
+    const [filteredClients, setFilteredClients] = useState([])
+    const [searchTerms, setSearchTerms] = useState("")
+    
+
+
+    const localLoggedinUser = localStorage.getItem("loggedin_user")
+    const loggedinUserObject = JSON.parse(localLoggedinUser)
+
+    const getAndSetAllClients = () => {
+        fetch("http://localhost:8088/patients")
+            .then(response => response.json())
+            .then((clientArray) => {
+                setClients(clientArray)
+            })
+    }
+
+    useEffect(() => {
+        getAndSetAllClients();
+    }, [])
+
+    useEffect(() => {
+        if (searchTerms === "") {
+            setFilteredClients(clients)
+        }
+    }, [clients])
+
+    useEffect(() => {
+
+        const searchedClients = clients.filter(obj => obj.name.toLowerCase()?.startsWith(searchTerms.toLowerCase()))
+        setFilteredClients(searchedClients)
+    }, [searchTerms])
+
+
+
+    if (searchTerms !== "") {
+
+        return (<>
+
+            <h2>Search Results</h2>
+            <article className="clients">
+                {
+                    filteredClients.map(client => {
+                        return (
+                            <section className="client" key={`client--${client.id}`}>
+                                <div className="client__header">🧑{client.name}</div>
+
+                                <div>Date of Birth: {client.dateOfBirth}</div>
+
+
+                            </section>
+                        )
+                    })
+                }
+            </article>
+        </>)
+    } else {
+
+        return <>
+
+
+            {loggedinUserObject.staff ?
+                <button onClick={() => navigate("/clients/create")}>Add Clients</button> : ""
+            }
+            <h2>List of Clients</h2>
+            <article className="clients">
+                {
+                    filteredClients.map(client => {
+                        return (
+                            <section className="client" key={`client--${client.id}`}>
+                                <div className="client__header">🧑{client.name}</div>
+
+                                <div>Date of Birth: {client.dateOfBirth}</div>
+
+                            </section>
+                        )
+                    })
+                }
+            </article>
+        </>
+    }
+
+
+
+}
