44395a0db6545c1a1f6a2a98d771258c75488f41
John Lu <johnlu2006@gmail.com>
2023-08-10 11:13:24 -0500

    access to homepage requiring login fixed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import { Authorized } from "./components/views/Authorized"
 import { ApplicationViews } from "./components/views/ApplicationViews"
 import { NavBar } from "./components/nav/NavBar"
 import { Login } from "./components/auth/Login"
 import { Register } from "./components/auth/Register"
 import './App.css';
+import { HomePage } from "./components/HomePage.js"
 
 function App() {
 	return <Routes>
 		<Route path="/login" element={<Login />} />
 		<Route path="/register" element={<Register />} />
+		<Route path="/" element={<Navigate to="/home" />} />
+		<Route path="/home" element={<HomePage />} />
 
 		<Route path="*" element={
 			<Authorized>
